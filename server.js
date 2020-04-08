d275dd8f4f9c2e0feeed756c1f5b862423945815
Yavdhesh <yavdheshsnchhr@gmail.com>
2020-04-08 21:58:30 +0530

    Your Message_

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var server = http.Server(app);
 var io=require('socket.io')(server);
 
 var users=[];
-server.listen(8082, function(){
+server.listen(8080, function(){
 	
 	console.log("Yavdhesh ri chaat application saale hai");
 });
