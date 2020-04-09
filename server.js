ee0c6041f17f8a2d159626ac0661e7f775fa98c2
Yavdhesh <yavdheshsnchhr@gmail.com>
2020-04-09 10:17:14 +0530

    prashnaa ki samkhyaa 10 se ab sabhi prashna aayenge

--- server.js
+++ server.js
@@ -306,7 +306,7 @@ console.log("Question Size");
 console.log(json.quiz_questions.length);
 var arr = [];
 var jsonToBe = {"quiz_questions":[]};
-while(arr.length < 10){
+while(arr.length < json.quiz_questions.length){
     var randomnumber = Math.floor(Math.random()*(json.quiz_questions.length));
     if(arr.indexOf(randomnumber) > -1 || nonrepeatingList.indexOf(randomnumber) > -1) 
 		continue;
